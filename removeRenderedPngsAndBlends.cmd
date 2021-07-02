4fe550d6957f5c4040e15fae07530caf099183b9
Eduardo dos Santos Leal <tcarvi@me.com>
2021-07-02 18:57:15 -0300

    Refactor

--- removeRenderedPngsAndBlends.cmd
+++ removeRenderedPngsAndBlends.cmd
@@ -1,2 +1,2 @@
-rm .\blender_projects\f1.blend && echo f1.blend excluded
+rm .\blender_projects\blenderFileName.blend && echo blenderFileName.blend excluded
 rm .\render_output\r1.png && echo r1.png excluded
